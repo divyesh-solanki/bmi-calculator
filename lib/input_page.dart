cd31f9164e8fa10a2864c08bded1dda7d091e7ad
divyesh <skdivyesh333@gmail.com>
2020-06-23 23:34:26 +0530

    created cards, setup theme

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+const bottomContainerHeight = 60.0; 
+const bottomContainerColor = Color(0xFFEB1555);
+const activeCardColor = Color(0xFF1D1F33);
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -12,12 +14,57 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(colour: activeCardColor),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+                Expanded(
+                  child: ReusableCard(colour: activeCardColor),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: bottomContainerHeight,
+            width: double.infinity,
+            margin: EdgeInsets.only(top: 10),
+            color: bottomContainerColor,
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {},
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour; // = Color(0xFF1D1F33);
+  ReusableCard({this.colour});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+        color: colour,
+        borderRadius: BorderRadius.circular(10.0),
       ),
     );
   }
